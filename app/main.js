a602442d779559644be4d3355448add2036292b0
Daniel Souza <danisztls@gmail.com>
2021-05-02 18:59:06 -0300

    Fix async problems

--- app/main.js
+++ app/main.js
@@ -1,15 +1,20 @@
 // Update UI to display count
-async function updateCount() {
-    let response = await fetch(url)
-    let count = response.text()
+async function updateIcon(count) {
     chrome.browserAction.setBadgeText({text:count})
     chrome.browserAction.setTitle({title:"" + count + " bookmarks (click to refresh)"})
+}
+
+// Update count of unread items
+async function getCount() {
+    let response = await fetch(url)
+    let count = await response.text()
+    await updateIcon(count)
 }
 
 const host = "https://localhost/tt-rss"
 const user = "admin"
 const url = host + "/public.php?op=getUnread&login=" + user
 
-chrome.browserAction.onClicked.addListener(updateCount)
+chrome.browserAction.onClicked.addListener(getCount)
 chrome.browserAction.setBadgeText({text:"."})
-updateCount()
+getCount
