35b73ea7606cb718d9b8e102cb33e684c762e128
Daniel Souza <danisztls@gmail.com>
2021-05-05 10:48:13 -0300

    Fix issues with loading/storing null data

--- app/options.js
+++ app/options.js
@@ -1,38 +1,55 @@
-let settings, hostInput, userInput
+// Declare global variables
+let settings = {
+    host: "",
+    user: "",
+    url:  ""
+}
+let hostInput, userInput
+
+// Load opts from local storage
+function loadOpts() {
+    // load data
+    // FIXME: Object need to be serialized
+    let _settings = localStorage.getItem('settings')
+    if (_settings) {
+        settings = _settings
+    }
+
+    // set defaults for null or undefined
+    if (!settings.host) {
+        settings.host = "https://localhost/tt-rss" 
+    }
+    if (!settings.user) {
+        settings.user = "admin" 
+    }
+    // reset url
+    settings.url = settings.host + "/public.php?op=getUnread&login=" + settings.user
+}
 
 // Save opts to local storage on click event
 function saveOpts() {
     // get data
+    // ignore null or undefined
     if (hostInput.value != '') {
         settings.host = hostInput.value
     }
     if (userInput.value != '') {
         settings.user = userInput.value
     }
-    
-    if (settings.user + settings.host) {
-        settings.url  = settings.host + "/public.php?op=getUnread&login=" + settings.user
-    }
+    // reset url
+    settings.url  = settings.host + "/public.php?op=getUnread&login=" + settings.user
 
     // store data
     localStorage.setItem('settings', settings)
 }
 
 window.onload = function main() {
-    // init object with defaults
-    settings = {
-        host: "https://localhost/tt-rss",
-        user: "admin",
-        url: "https://localhost/tt-rss/public.php?op=getUnread&login=admin"
-    }
+    // load opts from local storage
+    loadOpts()
 
     // get input elements
     hostInput = document.querySelector("input#opt-host")
     userInput = document.querySelector("input#opt-user")
-
-    // load stored data
-    // FIXME: It's overwriting defaults with blank when no opts are stored
-    settings = localStorage.getItem('settings')
     
     // update placeholders
     hostInput.placeholder = settings.host
@@ -41,8 +58,6 @@ window.onload = function main() {
     // monitor click event
     document.querySelector('input#opt-save').onclick = saveOpts
 }
-
-// TODO: Use a class for settings
 
 // TODO: Upgrade to Manifest v3
 // - Use chrome.storage and sync to save settings
