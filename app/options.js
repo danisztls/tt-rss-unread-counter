bbdeb96b5b9888e88f5ced128045165cb07f7062
Daniel Souza <danisztls@gmail.com>
2021-05-09 00:13:46 -0300

    Autoformat with ESLint/Standard

--- app/options.js
+++ app/options.js
@@ -1,6 +1,7 @@
 /* global chrome */
 
 // DATA
+// variables and classes are hoisted but not initialized if invoked before declaration
 const defaults = {
   host: 'https://localhost/tt-rss',
   user: 'admin',
@@ -8,9 +9,42 @@ const defaults = {
   interval: 15
 }
 
-let settings = [] // array of 'Setting' objects
+class Setting {
+  constructor (name, value) {
+    this.name = name
+    this.value = value
+    this.input = document.querySelector('#opt-' + this.name)
+  }
+
+  // set value
+  set (value) {
+    this.value = value
+  }
+
+  // update placeholder and clear input field
+  clear () {
+    this.input.placeholder = this.value
+    this.input.value = '' // clear input
+  }
+
+  // read value from input field
+  read () {
+    if (this.input.value != null) { // ignore null
+      this.set(this.input.value)
+      this.clear()
+    }
+    return [this.name, this.value]
+  }
+
+  // reset to default value
+  reset () {
+    this.value = defaults[this.name]
+    this.clear()
+  }
+}
 
 // init 'Setting' objects
+const settings = []
 for (const opt of Object.entries(defaults)) {
   settings.push(new Setting(opt[0], opt[1]))
 }
@@ -21,47 +55,13 @@ getOpts(defaults)
 
 // Monitor click events
 window.onload = () => {
-    document.querySelector("#opt-save").onclick = saveOpts
-    document.querySelector("#opt-reset").onclick = resetOpts
+  document.querySelector('#opt-save').onclick = saveOpts
+  document.querySelector('#opt-reset').onclick = resetOpts
 }
 
 // LIB
-class Setting {
-    constructor(name, value) {
-        this.name = name
-        this.value = value
-        this.input = document.querySelector("#opt-" + this.name)
-    }
-
-    // set value
-    set(value) {
-      this.value = value
-    }
-
-    // update placeholder and clear input field
-    clear() {
-      this.input.placeholder = this.value
-      this.input.value = "" // clear input
-    }
-
-    // read value from input field
-    read() {
-        if (this.input.value != null) { // ignore null
-            this.set(this.input.value)
-            this.clear()
-        }
-        return this.name, this.value
-    }
-    
-    // reset to default value
-    reset() {
-      this.value = defaults[this.name]
-      this.clear()
-    }
-}
-
 // Load settings from chrome.storage
-function getOpts(query) {
+function getOpts (query) {
   const data = new Promise((resolve, reject) => {
     try {
       chrome.storage.sync.get(query, resolve)
@@ -73,25 +73,27 @@ function getOpts(query) {
 }
 
 // Set value for each option
-async function setOpt(data) {
-  for (let opt of settings) {
+async function setOpt (data) {
+  for (const opt of settings) {
     opt.set(data[opt.name])
   }
 }
 
 // Save settings to chrome.storage
-function saveOpts() {
-  let payload = new Object()
-  for (let opt of settings) {
-    let name, value = opt.read()
+function saveOpts () {
+  const payload = new Object() // eslint-disable-line no-new-object
+  for (const opt of settings) {
+    const data = opt.read()
+    const name = data[0]
+    const value = data[1]
     payload[name] = value
   }
   chrome.storage.sync.set(payload) // store opt
 }
 
 // Reset settings to default values
-async function resetOpts() {
-  for (let opt of settings) {
+async function resetOpts () {
+  for (const opt of settings) {
     opt.reset()
   }
   chrome.storage.sync.set(defaults)
